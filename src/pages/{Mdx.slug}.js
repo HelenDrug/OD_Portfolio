a6fea66c6eaf48299766f28ae3f33a5382d8b07a
HelenDrug <helendrugalya@gmail.com>
2021-03-23 09:58:55 +0100

    added GatsbyImage to the post

--- src/pages/{Mdx.slug}.js
+++ src/pages/{Mdx.slug}.js
@@ -2,13 +2,17 @@ import React from "react";
 import { graphql } from "gatsby";
 import { MDXRenderer } from "gatsby-plugin-mdx";
 import Layout from "../components/layout";
+import { GatsbyImage, getImage } from "gatsby-plugin-image";
 
 const BlogPostPage = ({ data }) => {
   const post = data.mdx;
+  const image = getImage(post.frontmatter.image);
+  const alt = post.frontmatter.imageAlt;
 
   return (
     <Layout>
       <article>
+        <GatsbyImage image={image} alt={alt} />
         <h1>{data.mdx.frontmatter.title}</h1>
         <MDXRenderer>{post.body}</MDXRenderer>
       </article>
@@ -21,6 +25,12 @@ export const query = graphql`
     mdx(id: { eq: $id }) {
       frontmatter {
         title
+        image {
+          childImageSharp {
+            gatsbyImageData(layout: CONSTRAINED)
+          }
+        }
+        imageAlt
       }
       body
     }
